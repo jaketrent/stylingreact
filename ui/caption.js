2ce9cfe5fe6cf94598203da2085e98d84b9e87f7
Anne Trent <anne.trent1@gmail.com>
2017-08-10 14:19:42 -0600

    Add caption

--- ui/caption.js
+++ ui/caption.js
@@ -1,22 +1,20 @@
 export default _ =>
   <div className="caption">
     <p>A comparison of CSS approaches in React</p>
-    <style jsx>
-      {' '}{`
+    <style jsx>{`
+      .caption {
+        text-align: center;
+        font-size: 1.3em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+      }
+      p {
+        margin: 0;
+      }
+      @media screen and (min-width: 1441px) {
         .caption {
-          text-align: center;
-          font-size: 1.3em;
-          text-transform: uppercase;
-          letter-spacing: 1px;
+          font-size: 1em;
         }
-        p {
-          margin: 0;
-        }
-        @media screen and (min-width: 1441px) {
-          .caption {
-            font-size: 1em;
-          }
-        }
-      `}{' '}
-    </style>
+      }
+    `}</style>
   </div>
