dab066fd655a39f8fef31a78ca6217f668264e79
Anne Trent <anne.trent1@gmail.com>
2017-08-14 11:19:38 -0600

    fix section padding

--- ui/section.js
+++ ui/section.js
@@ -4,7 +4,7 @@ export default props =>
     <style jsx>{`
       .section {
         max-width: 550px;
-        padding: 35px 10px 10px 30px;
+        padding: 35px 10px 30px 10px;
       }
       @media screen and (min-width: 769px) {
         .section {
