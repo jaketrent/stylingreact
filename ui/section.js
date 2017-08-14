0d3b55a7a4ad532c3238d11d3ccb279d5b7b7e4d
Anne Trent <anne.trent1@gmail.com>
2017-08-14 10:38:59 -0600

    sections have 2 columns starting at 769px

--- ui/section.js
+++ ui/section.js
@@ -3,7 +3,14 @@ export default props =>
     {props.children}
     <style jsx>{`
       .section {
-        max-width: 540px;
+        max-width: 550px;
+      }
+      @media screen and (min-width: 769px) {
+        .section {
+          max-width: 50%;
+        }
+      }
+      @media screen and (min-width: 1441px) {
       }
     `}</style>
   </div>
