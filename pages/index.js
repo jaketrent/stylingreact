093c479e05183a0902bb74af81081cd03d695c2f
jaketrent <trent.jake@gmail.com>
2017-08-16 06:31:25 -0600

    real copy for techs

--- pages/index.js
+++ pages/index.js
@@ -29,24 +29,49 @@ export default _ =>
       <Main>
         <Section>
           <SectionHeader icon={<InlineCss />}>Inline Styles</SectionHeader>
-          <SectionBody src="http://lorempixel.com/500/300/abstract/1" />
+          <SectionBody src="http://lorempixel.com/500/300/abstract/1">
+            In the traditional world of CSS, inline styles are a no-no. But in
+            the new world of components in UIs, inline styles have some
+            advantages. Christopher Chedeau gave his exposition on what problems
+            inline styles solve. We cover that talk and what it means for us in
+            this section. Demonstrated here is the most basic form of inline
+            styles possible.
+          </SectionBody>
         </Section>
 
         <Section>
           <SectionHeader icon={<Radium />}>Radium</SectionHeader>
-          <SectionBody src="http://lorempixel.com/500/300/abstract/2/" />
+          <SectionBody src="http://lorempixel.com/500/300/abstract/2/">
+            Radium provides a step up from vanilla inline styles. By using this
+            library you get extra features available for use that you didn't
+            have before. Radium is one of the pioneers in this enhanced inline
+            styles space that has given rise to a string of similar libraries
+            for CSS in JS.
+          </SectionBody>
         </Section>
 
         <Section>
           <SectionHeader icon={<ExternalCss />}>
             External Stylesheet
           </SectionHeader>
-          <SectionBody src="http://lorempixel.com/500/300/abstract/3" />
+          <SectionBody src="http://lorempixel.com/500/300/abstract/3">
+            If you're just getting into React and want to keep your CSS story as
+            it has been traditionally, regular CSS in an external stylesheet
+            will feel super familiar. This route will ensure you can use your
+            tried and true styling strategies that you've developed over time.
+            You'll also get a chance to play with some a new tool, Webpack, to
+            get those styles on the page.
+          </SectionBody>
         </Section>
 
         <Section>
           <SectionHeader icon={<CssModules />}>CSS Modules</SectionHeader>
-          <SectionBody src="http://lorempixel.com/500/300/abstract/4" />
+          <SectionBody src="http://lorempixel.com/500/300/abstract/4">
+            Once you taste CSS modules, there will be no going back. In a
+            surprisingly-simple, genius way, CSS modules allow you to separate
+            and compartmentalize your CSS. This allows you to make isolated
+            changes to styles without affecting the world. It's life changing.
+          </SectionBody>
         </Section>
 
         <Section>
--- ui/section-body.js
+++ ui/section-body.js
@@ -1,11 +1,7 @@
 export default props =>
   <div className="sectionBody">
     <p className="sectionParagraph">
-      Run! Yes. A Jedi's strength flows from the Force. But beware of the dark
-      side. Anger...fear...aggression. The dark side of the Force are they.
-      Easily they flow, quick to join you in a fight. Vader. Is the dark side
-      stronger? No...no...no. But how am I to know the good side from the bad?
-      You will know. But tell me why I can't... No, no, there is no why.
+      {props.children}
     </p>
     <img className="sectionPicture" src={props.src} />
     <style jsx>{`
