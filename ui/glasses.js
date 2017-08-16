b43a8cdc8c91c663811dea3cfbcbad2de1d52d7c
jaketrent <trent.jake@gmail.com>
2017-08-16 07:04:14 -0600

    initialize glasses in hidden space; stay on last frame of animation

--- ui/glasses.js
+++ ui/glasses.js
@@ -9,15 +9,10 @@ export default _ =>
       }
       .glassesImg {
         width: 100%;
-        animation: fall 5s 1s;
+        animation: fall 5s 0.5s forwards;
+        transform: translate3d(0, -500px, 0);
       }
       @keyframes fall {
-        0% {
-          transform: translate3d(0, -500px, 0);
-        }
-        70% {
-          transform: translate3d(0, 0, 0);
-        }
         100% {
           transform: translate3d(0, 0, 0);
         }
