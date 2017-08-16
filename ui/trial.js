8999b9d96d09514e42db433373ed8daad31725a5
jaketrent <trent.jake@gmail.com>
2017-08-16 07:13:21 -0600

    adjust button styles

--- ui/trial.js
+++ ui/trial.js
@@ -1,34 +1,31 @@
 export default _ =>
   <div className="trial">
-    <div className="button">
-      <a
-        className="signUpLink"
-        href="https://www.pluralsight.com/courses/react-styling-components"
-      >
-        Try Pluralsight
-      </a>
-    </div>
+    <a
+      className="link"
+      href="https://www.pluralsight.com/courses/react-styling-components"
+    >
+      Try Pluralsight
+    </a>
     <p className="caption">free for 10 days</p>
     <style jsx>{`
-      .signUpLink {
+      .link {
         border-bottom: none;
         border-radius: 3px;
         background-color: #61dafb;
-        padding: 1em 1.7em;
-        font-size: 1.2em;
+        padding: 1em 1.75em;
+        font-size: 1.25em;
         text-transform: uppercase;
-        text-shadow: -3px -1px 6px black;
-        box-shadow: -5px -5px 4px black;
+        text-shadow: -1px -1px 0 blue;
+        font-family: 'Libre Franklin', sans-serif;
       }
-      .signUpLink:hover {
+      .link:hover {
         background-color: #4ed6fb;
         color: #f5f5f5;
-        text-shadow: -1.5px -1px 6px black;
-        box-shadow: none;
       }
       .caption {
-        margin: 0;
+        margin: 0.675em;
         text-align: center;
+        font-size: 0.675em;
       }
     `}</style>
   </div>
