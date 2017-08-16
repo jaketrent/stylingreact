abff166fe2d0094d37d5e124a6f4eb1f624848ce
Anne Trent <anne.trent1@gmail.com>
2017-08-16 11:20:24 -0600

    add table of contents

--- ui/list-item.js
+++ ui/list-item.js
@@ -1,1 +1,41 @@
-export default props => <div className="listItem" />
+export default props =>
+  <div className="listItem">
+    <a className="name">
+      {props.name}
+    </a>
+    <p className="time">
+      {props.time}
+    </p>
+    <style jsx>{`
+        .listItem {
+            display: flex;
+            justify-content: space-between;
+            margin: 1em 0;
+
+        }
+        .name {
+            border-bottom: 1px solid #61dafb;
+            margin-right: 2.2em;
+        }
+        .time {
+            margin: 0;
+        }
+        @media screen and (min-width: 426px) {
+            .name {
+                margin-right: 4.5em;
+            }
+        }
+        @media screen and (min-width: 769px) {
+            .name {
+                margin-right: 2em;
+            }
+        }
+        }
+        @media screen and (min-width: 1441px) {
+            .name {
+                margin-right: 6em;
+            }
+        }
+    }
+    `}</style>
+  </div>
