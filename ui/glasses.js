d579d3800d7bb18485ef93a832a97ead31ef3ad9
jaketrent <trent.jake@gmail.com>
2017-08-16 06:47:04 -0600

    remove glasses bounce, make fall straight

--- ui/glasses.js
+++ ui/glasses.js
@@ -4,25 +4,19 @@ export default _ =>
     <style jsx>{`
       .glasses {
         position: absolute;
-        width: auto;
-        height: auto;
         z-index: 1;
         margin: 18% 20vw 18% 20vw;
       }
       .glassesImg {
         width: 100%;
-        animation-name: bounceDown;
-        animation-duration: 5s;
+        animation: fall 5s 1s;
       }
-      @keyframes bounceDown {
+      @keyframes fall {
         0% {
-          transform: translate3d(-150px, -500px, 0);
+          transform: translate3d(0, -500px, 0);
         }
         70% {
           transform: translate3d(0, 0, 0);
-        }
-        84% {
-          transform: translate3d(0, -10px, 0);
         }
         100% {
           transform: translate3d(0, 0, 0);
