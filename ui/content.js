4d455db303363d7de7ffba95fd5372a4f9e9e7d7
Anne Trent <anne.trent1@gmail.com>
2017-08-23 13:43:26 -0600

    google analytics works

--- ui/content.js
+++ ui/content.js
@@ -1,13 +1,18 @@
+import GaWrapper from './ga-wrapper'
 export const padding = '20px'
+const Content = props => {
+  return (
+    <div className="content">
+      {props.children}
+      <style jsx>{`
+        .content {
+          max-width: 1200px;
+          margin: auto;
+          padding: ${padding};
+        }
+      `}</style>
+    </div>
+  )
+}
 
-export default props =>
-  <div className="content">
-    {props.children}
-    <style jsx>{`
-      .content {
-        max-width: 1200px;
-        margin: auto;
-        padding: ${padding};
-      }
-    `}</style>
-  </div>
+export default GaWrapper(Content)
